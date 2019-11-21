723a3d9ff2f5926e72ab4e2383bdf8a1f0212ba6
Ana <ana.caldeira@sagatechbrasil.com.br>
2019-11-20 21:51:49 -0300

    Alteração no layout para exibir lista de convidados e acréscimo do botão para adicionar novos convidados.

--- app/src/main/java/com/example/eventos/MainActivity.java
+++ app/src/main/java/com/example/eventos/MainActivity.java
@@ -3,17 +3,48 @@ package com.example.eventos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.TextureView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class MainActivity extends AppCompatActivity {
+    ListView listItemView;
+    FloatingActionButton fab;
+
+    String[] listItemsValue = new String[] {
+            "Android",
+            "PHP",
+            "Web Development",
+            "Blogger",
+            "SEO",
+            "Photoshop",
+            "Android Studio",
+            "Eclipse",
+            "SDK Manager",
+            "AVD Manager"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView convidado = new TextView(this);
-        convidado.setText("Ana Maria");
-        setContentView(convidado);
+        setContentView(R.layout.list_convidados);
+
+        listItemView = (ListView)findViewById(R.id.listView);
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1, listItemsValue);
+
+        listItemView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "Fab Clicked", Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
 }
